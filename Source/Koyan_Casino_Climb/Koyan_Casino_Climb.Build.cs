3ab7f0da6f0e805324b826f488023a5724b8f6fe
Walkman <patrykwalkowiak58@gmail.com>
2025-12-21 21:34:17 +0100

    tto co mam

--- Source/Koyan_Casino_Climb/Koyan_Casino_Climb.Build.cs
+++ Source/Koyan_Casino_Climb/Koyan_Casino_Climb.Build.cs
@@ -8,9 +8,9 @@ public class Koyan_Casino_Climb : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Paper2D", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities","GameplayTags","GameplayTasks" });
+		// PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities","GameplayTags","GameplayTasks" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
